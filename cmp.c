1053533e8b68bd23c7d168d3c412c61cb6da5103
NisimAtiya <118813292+NisimAtiya@users.noreply.github.com>
2023-04-07 17:20:00 +0300

    #2 -  cmp tool done

--- cmp.c
+++ cmp.c
@@ -1,90 +1,83 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <sys/stat.h>
+// Function to calculate file size
+long int get_file_size(const char* filename) {
+    FILE* fp = fopen(filename, "rb"); // Open file in binary mode
+    if (fp == NULL) {
+        return -1; // Error opening file
+    }
+    fseek(fp, 0, SEEK_END); // Seek to end of file
+    long int size = ftell(fp); // Get current position (which is the file size)
+    fclose(fp); // Close file
+    return size;
+}
 
-int cmp(const char* file1, const char* file2, int verbose, int ignore_case) {
+int cmp(const char* file1, const char* file2, int v, int ignore_case) {
+    //Checks if the sizes are the same
+    if (get_file_size(file1)!=get_file_size(file2)){
+        if (v) {
+            printf("distinct\n");
+        }
+        return 1;
+    }
+    // If the files are empty then they are equal
+    if(get_file_size(file1)==0){
+        if (v) {
+            printf("equal\n");
+        }
+        return 0;
+    }
+    // opening the files
     FILE *f1, *f2;
     char c1, c2;
-
     f1 = fopen(file1, "r");
     if (f1 == NULL) {
         printf("Error: failed to open %s\n", file1);
         return 2;
     }
-
     f2 = fopen(file2, "r");
     if (f2 == NULL) {
         printf("Error: failed to open %s\n", file2);
         fclose(f1);
         return 2;
     }
-    // Calculate the size of file1
-    struct stat st1;
-    int fd1 = fileno(file1);    // get file descriptor
-    fstat(fd1, &st1);
-    off_t size_file1 = st1.st_size;
-    // Calculate the size of file2
-    struct stat st2;
-    int fd2 = fileno(file1);    // get file descriptor
-    fstat(fd2, &st2);
-    off_t size_file2= st2.st_size;
-    //Checks if the sizes are the same
-    if (size_file1!=size_file2){
-        fclose(f1);
-        fclose(f2);
-        if (verbose) {
-            printf("distinct\n");
-        }
-        return 1;
-    }
 
 
-    while ((c1 = fgetc(f1)) != EOF || (c2 = fgetc(f2)) != EOF) {
+    for (long int i = 0; i <get_file_size(file1) ; ++i) {
+        c1 = fgetc(f1);
+        c2 = fgetc(f2);
+        // changes to lower case if ignore_case is on
         if (ignore_case) {
-            if( (c2>'a' && c1<'z' || c1>'A' && c1<'Z') && (c2>'a'&&c1<'z'||c1>'A'&&c1<'Z') ){
-                c1 = tolower(c1);
-                c2 = tolower(c2);
-            }
-
+            c1 = tolower(c1);
+            c2 = tolower(c2);
 
         }
+        // Checks if the chars are different
         if (c1 != c2) {
             fclose(f1);
             fclose(f2);
-            if (verbose) {
-                printf("nisim\n");
+            // Prints distinct if v is on and returns 1
+            if (v) {
                 printf("distinct\n");
             }
             return 1;
         }
     }
-
-    if (c1 != EOF || c2 != EOF) {
-        fclose(f1);
-        fclose(f2);
-        if (verbose) {
-            printf("atiya\n");
-            printf("distinct\n");
-        }
-        return 1;
-    }
-
-    fclose(f1);
-    fclose(f2);
-
-    if (verbose) {
+    // Prints equal if v is on and returns 0
+    if (v) {
         printf("equal\n");
     }
 
     return 0;
+
 }
 
 int main(int argc, char *argv[]) {
-    int verbose = 0;
+    // bits for user input for -v and -i
+    int v = 0;
     int ignore_case = 0;
-
+    // checking if input is not correct
     if (argc < 3 || argc > 5) {
         printf("Usage: cmp <file1> <file2> [-v] [-i]\n");
         return 2;
@@ -92,7 +85,7 @@ int main(int argc, char *argv[]) {
 
     if (argc == 4) {
         if (strcmp(argv[3], "-v") == 0) {
-            verbose = 1;
+            v = 1;
         } else if (strcmp(argv[3], "-i") == 0) {
             ignore_case = 1;
         } else {
@@ -103,10 +96,10 @@ int main(int argc, char *argv[]) {
 
     if (argc == 5) {
         if (strcmp(argv[3], "-v") == 0 && strcmp(argv[4], "-i") == 0) {
-            verbose = 1;
+            v = 1;
             ignore_case = 1;
         } else if (strcmp(argv[3], "-i") == 0 && strcmp(argv[4], "-v") == 0) {
-            verbose = 1;
+            v = 1;
             ignore_case = 1;
         } else {
             printf("Unknown combination of flags: %s %s\n", argv[3], argv[4]);
@@ -114,5 +107,5 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    return cmp(argv[1], argv[2], verbose, ignore_case);
+    return cmp(argv[1], argv[2], v, ignore_case);
 }
